4242cc2b9d309e602e6c9de57b5497f616ff2e48
Leo Liu <liuyuze1993@qq.com>
2015-11-25 23:55:30 +1300

    1

---
+++ ConsotoUniWebApp/ConsotoUniWebApp/Scripts/CourseModule.js
@@ -0,0 +1,61 @@
+﻿var CourseModule = (function () {
+
+    return {
+        getCourses: function (callback) {
+            $.ajax({
+                type: "GET",
+                dataType: "json",
+                url: "http://consotoapileo.azurewebsites.net/api/Courses",
+                success: function (data) {
+                    callback(data);
+                }
+            });
+        },
+
+        getCourseById: function (id, callback) {
+            $.ajax({
+                type: "GET",
+                dataType: "json",
+                url: "http://consotoapileo.azurewebsites.net/api/Courses/" + id,
+                success: function (data) {
+                    callback(data);
+                }
+            });
+        },
+
+        addCourse: function (Course, callback) {
+            $.ajax({
+                type: "POST",
+                data: Course,
+                url: "http://consotoapileo.azurewebsites.net/api/Courses",
+                success: function (data, textStatus, jqXHR) {
+                    callback();
+                }
+            });
+        },
+
+        updateCourse: function (Courseid, Course, callback) {
+            $.ajax({
+                type: "PUT",
+                data: Course,
+                url: "http://consotoapileo.azurewebsites.net/api/Courses/" + Courseid,
+                success: function (data, textStatus, jqXHR) {
+                    callback();
+                }
+            });
+        },
+
+        deleteCourse: function (Courseid, callback) {
+            $.ajax({
+                type: "DELETE",
+                dataType: "json",
+                url: "http://consotoapileo.azurewebsites.net/api/Courses/" + Courseid,
+                success: function (data) {
+                    callback();
+                }
+            });
+        }
+
+    };
+
+}());
--- ConsotoUniWebApp/ConsotoUniWebApp/Scripts/Enrollment.js
+++ ConsotoUniWebApp/ConsotoUniWebApp/Scripts/Enrollment.js
@@ -1,13 +1,38 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
-    var StudentId = getUrlParameters("id", "", true);
-    StudentModule.getStudentById(StudentId, function (Student) {
+    CurrentStudentId = getUrlParameters("id", "", true);
+    StudentModule.getStudentById(CurrentStudentId, function (Student) {
         var FullName = Student["FirstName"] + " " + Student["LastName"] + " : Enrolled Courses ";
         document.getElementById("StudentInfo").innerHTML = FullName;
         document.getElementById("StudentInfo").classList.remove("hidden");
     });
-    alert("1");
+
     EnrollmentModule.getEnrollments(function (Enrollments) {
-        alert("2");
+        var TotalEnrolled = 0;
+        for (i = 0; i < Enrollments.length; i++) {
+            if (Enrollments[i].StudentID == CurrentStudentId) {
+                TotalEnrolled++;
+                var CurrentCourseId = Enrollments[i].CourseID;
+                CourseModule.getCourseById(CurrentCourseId, function (Course) {
+                    var CurrentRow = document.createElement('tr');
+                    CurrentRow.setAttribute("data-id", CurrentCourseId);
+
+                    var TitleCol = document.createElement('td');
+                    TitleCol.innerHTML = Course["Title"];
+                    CurrentRow.appendChild(TitleCol);
+
+                    var CreditsCol = document.createElement('td');
+                    CreditsCol.innerHTML = Course["Credits"];
+                    CurrentRow.appendChild(CreditsCol);
+
+                    var CompletionStatusCol = document.createElement('td');
+                    CompletionStatusCol.innerHTML = Course["CompletionStatus"];
+                    CurrentRow.appendChild(CompletionStatusCol);
+
+                    document.getElementById("OutputEnrollmentList").appendChild(CurrentRow);
+                });
+            };
+        };
+        document.getElementById("TotalEnrolled").innerHTML = "The Total of Enrolled Courses: "+ TotalEnrolled;
     });
     
 });
